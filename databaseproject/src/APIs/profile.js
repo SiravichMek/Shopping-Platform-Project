8b9a293b852ec800fa5b91d21d1e7a342cda0265
Chawanakon <6422770246@g.siit.tu.ac.th>
2023-10-28 13:56:41 +0700

    test 0.3567

--- databaseproject/src/APIs/profile.js
+++ databaseproject/src/APIs/profile.js
@@ -9,13 +9,15 @@ router_profile.post('/api/profile', async (req, res) => {
   const query = 'SELECT * FROM users WHERE Username = ? AND Password = ?';
   const values = [Username, Password];
   const db = await createConnection();
-    
+  
+  console.log(req.body)
     try {
       const results = await db.query(query, values);
       if(results[0].length==0){
         res.status(500).json({ error: 'Not have related user ID' })
       }
       else{
+        
       res.status(200).json({ data: results });
       console.log(results)}
     } catch (error) {
--- databaseproject/src/Components/Profileinform.js
+++ databaseproject/src/Components/Profileinform.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const apiUrl = 'http://localhost:3001/api/createUser'; 
+const apiUrl = 'http://localhost:3001/api/profile'; 
 
 const Profileform =() =>{
-    const [name, setName] = useState('');
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const [address, setAddress] = useState('');
-    const [telephone, setTelephone] = useState('');
-
-    const createUser = async () => {
+    const [responseData, setResponseData] = useState(null);
+    
+    useEffect(() => {
+        Logintest();
+      }, []);
+      
+    const Logintest = async () => {
         try {
           const response = await axios.post(apiUrl, {
             Username: sessionStorage.getItem('Username'),
-            password: sessionStorage.getItem('Password'),
+            Password: sessionStorage.getItem('Password'),
           });
           console.log('API Response:', response.data);
           // handle success or redirection here
@@ -22,15 +22,21 @@ const Profileform =() =>{
           // handle error here
         }
       };
+
+      
     return( 
     <>
         <h1>Profile Page</h1>
             <div className="container">
-                <h1>Username: {sessionStorage.getItem('Username')}</h1>
+                
                 <div className="row-md-4">
                     <div className="col-md-12">
                         Profile Information
                     </div>
+                    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+      
+    </div>
                 </div>
             </div>
     </>
