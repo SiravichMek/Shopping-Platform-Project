812a81f399131e8fadf3413c52f1d79c29a9538e
siravichMek <6422782704@g.siit.tu.ac.th>
2023-10-28 13:21:24 +0700

    add profile api

--- databaseproject/src/APIs/api.js
+++ databaseproject/src/APIs/api.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 // import router
 import router_login from "./login.js";
 import router_regist from "./regist.js";
+import router_profile from "./profile.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,7 @@ app.use(cors());
 // Add api to server port 3001
 app.use(router_login);
 app.use(router_regist);
+app.use(router_profile);
 
 app.listen(3001, () => {
     console.log(`> Ready on http://localhost:3001`);
