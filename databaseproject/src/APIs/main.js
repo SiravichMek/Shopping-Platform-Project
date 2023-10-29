e75c879b172e161fc32cc199013a2765cb1b3f40
siravichMek <6422782704@g.siit.tu.ac.th>
2023-10-29 14:49:07 +0700

    ,,,,

--- databaseproject/src/APIs/main.js
+++ databaseproject/src/APIs/main.js
@@ -5,13 +5,14 @@ import createConnection from './connect.js';
 const router_main = express.Router();
 
 router_main.get('/api/main', async (req, res) => {
-  const query = 'SELECT * FROM products';
+  
   const db = await createConnection();
     
     try {
+      const query = 'SELECT * FROM products';
       const results = await db.query(query);
-      res.status(200).json({ data: results });
-      console.log(results)
+      res.status(200).json({ data: results[0] });
+      console.log(results[0])
     } catch (error) {
       console.error('Database Error:', error);
       res.status(500).json({ error: 'Occur some conflict with retrieving process' });
