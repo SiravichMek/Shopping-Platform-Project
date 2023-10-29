e7c09ab8f9947354b3d2a7e0bb0dea4a191190e2
siravichMek <6422782704@g.siit.tu.ac.th>
2023-10-29 21:18:10 +0700

    edit profile api

--- databaseproject/src/APIs/api.js
+++ databaseproject/src/APIs/api.js
@@ -8,6 +8,8 @@ import router_regist from "./regist.js";
 import router_profile from "./profile.js";
 import router_shop from "./shop.js";
 import router_main from "./main.js"
+import { router_fetchProfile } from "./editProfile.js";
+import { router_updateProfile } from "./editProfile.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +21,8 @@ app.use(router_regist);
 app.use(router_profile);
 app.use(router_shop);
 app.use(router_main);
+app.use(router_fetchProfile);
+app.use(router_updateProfile);
 
 app.listen(3001, () => {
     console.log(`> Ready on http://localhost:3001`);
