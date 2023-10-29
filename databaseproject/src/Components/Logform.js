7f5f84d94dd3483ff9089d0c467c86f7264e4875
Chawanakon <6422770246@g.siit.tu.ac.th>
2023-10-30 05:58:19 +0700

    commit(add css)0.5

--- databaseproject/src/Components/Logform.js
+++ databaseproject/src/Components/Logform.js
@@ -26,9 +26,9 @@ const LoginForm = () => {
           icon: 'success',
           title: 'Login succeeded',
           showConfirmButton: false,
-          timer: 800
+          timer: 1000
         })
-        setTimeout(() => {window.location.href = '/main';}, 1000);
+        setTimeout(() => {window.location.href = '/main';}, 1500);
          
        }
       
@@ -117,7 +117,7 @@ const LoginForm = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                  Login
                 </button>
--- databaseproject/src/Components/Maincompo.js
+++ databaseproject/src/Components/Maincompo.js
@@ -6,43 +6,43 @@ const apiUrl = 'http://localhost:3001/api/main';
 
 const Maincompo = () => {
     const [responseData, setResponseData] = useState([]);
+    const [imageData, setImageData] = useState(null);
+    const [error, setError] = useState(null);
 
     const getProducts = async () => {
         try {
-            const response = await axios.get(apiUrl);
+            const response = await axios.get(apiUrl)
+            
             setResponseData(response.data.data);
             console.log(response.data.data);
+            
+            // console.log(response.data.data);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
-
     useEffect(() => {
         getProducts();
     }, []);
-
+    
     return (
         <>
-            <h2>Data from Backend:</h2>
-            <div className="">
-                
-                {responseData.map((products) => (
-                    <div key={products.ProductID} className="">
-                        <div className="">
-                                <div className="">
-                                <div className="">
-                                    <img src={products.Picture} />
-                                </div>
-
-                                <div className="">
-                                    <p>Product name: {products.Name}</p>
-                                    <p>Description: {products.Description}</p>
-                                    <p>Price: {products.Cost}</p>
-                                </div>
-                            </div>
+            
+            <div  className="container mx-auto w-full ">
+            
+                <div className=" grid grid-cols-5 justify-items-center ">
+                    {responseData.map((products, index) => (
+                    <div key={index} className=" w-3/4 place-content-center p-auto shadow-lg  my-10">
+                            <img src='https://idea7.co.uk/wp-content/uploads/2021/02/placeholder-250x250-1.png'/>
+                            {/* <img src='https://dummyimage.com/300.png/09f/fff'/> */} 
+                            <div className="text-center ">
+                            <h3 className='text-xl mt-3'>{products.Name}</h3>
+                            {/* <p>Description: {products.Description}</p> */}
+                            <p className='text-2xl my-5'>฿{products.Cost}</p>
                         </div>
                     </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </>
     );
